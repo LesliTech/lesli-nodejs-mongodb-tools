d655888dfee57d045ff4e818415e2f8370a2b218
Luis Donis <ldonis.emc2@gmail.com>
2019-08-05 20:27:01 -0600

    Basic operations

--- src/lesli-mongodb-wrapper.js
+++ src/lesli-mongodb-wrapper.js
@@ -37,6 +37,7 @@ const ObjectId = MongoDB.ObjectID
 // ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~     ~·~
 class databaseService {
 
+
     //
     constructor(config) {
         this.config = config
@@ -46,6 +47,7 @@ class databaseService {
         this.namespace = this.config.namespace
     }
 
+
     // · 
     getSchema(database) {
 
@@ -53,229 +55,325 @@ class databaseService {
 
         return this.connection.then(e => {
 
-            let schema = this.client.db(database.schema);
+            let schema = this.client.db(database.schema)
 
-            let collection = schema.listCollections();
+            let collection = schema.listCollections()
 
             return new Promise((resolve, reject) => {
 
-                return resolve(collection.toArray());
+                return resolve(collection.toArray())
 
-            });
+            })
 
         }).catch(error => {
 
-            console.log(error);
+            console.log(error)
             
-        });
+        })
+
     }
+
 
     // · 
     getSchemaCollection(database) {
-        database.schema = [this.namespace, database.schema].join('-');
+
+        database.schema = [this.namespace, database.schema].join('-')
+
         return this.connection.then(e => {
-            let schema = this.client.db(database.schema);
-            let collection = schema.collection(database.collection);
-            return collection.stats();
+
+            let schema = this.client.db(database.schema)
+            let collection = schema.collection(database.collection)
+            return collection.stats()
+
         }).catch(error => {
-            console.log(error);
-        });
+
+            console.log(error)
+
+        })
+
     }
+
 
     // · 
     createSchemaCollection(database) {
-        database.schema = [this.namespace, database.schema].join('-');
+
+        database.schema = [this.namespace, database.schema].join('-')
+
         return this.connection.then(e => {
-            let schema = this.client.db(database.schema);
-            return schema.createCollection(database.collection);
+
+            let schema = this.client.db(database.schema)
+            return schema.createCollection(database.collection)
+
         }).catch(error => {
-            console.log(error);
-        });
+
+            console.log(error)
+
+        })
+
     }
+
 
     // · 
     dropSchemaCollection(database) {
-        database.schema = [this.namespace, database.schema].join('-');
+
+        database.schema = [this.namespace, database.schema].join('-')
+
         return this.connection.then(e => {
-            let schema = this.client.db(database.schema);
-            let collection = schema.collection(database.collection);
-            return collection.drop();
+
+            let schema = this.client.db(database.schema)
+            let collection = schema.collection(database.collection)
+            return collection.drop()
+
         }).catch(error => {
-            console.log(error);
-        });
+
+            console.log(error)
+
+        })
+
     }
+
+
     // · 
     getSchemaCollectionDocuments(database) {
-        let pipeline = this.aggregationPipelineSelection({});
-        database.schema = [this.namespace, database.schema].join('-');
-        return this.aggregate(database, pipeline);
+
+        database.schema = [this.namespace, database.schema].join('-')
+
+        let pipeline = this.aggregationPipelineSelection({})
+        return this.aggregate(database, pipeline)
+
     }
+
+
+    // · Find documents
+    findDocument(database, options = {}) {
+
+        database.schema = [this.namespace, database.schema].join('-')
+
+        // parse specific document id
+        if (options.query._id) {
+            options.query._id = ObjectId(options.query._id)
+        }
+
+        return this.connection.then(e => {
+
+            let schema = this.client.db(database.schema)
+            let collection = schema.collection(database.collection)
+
+            return collection
+                .find(options.query)
+                .project(options.fields)
+                .sort(options.sort)
+                .toArray()
+
+        }).catch(error => {
+
+            console.log(error)
+            
+        })
+
+    }
+
+    // · Update specific document
+    updateDocument(database, options = {}, data = {}) {
+
+        database.schema = [this.namespace, database.schema].join('-')
+
+        // Advanced search options
+        if (!options.query) {
+            options.query = {}
+        }
+
+        // Find by object id
+        if (options.query._id) {
+            options.query._id = ObjectId(options.query._id)
+        }
+
+        return this.connection.then(e => {
+
+            let schema = this.client.db(database.schema)
+            let collection = schema.collection(database.collection)
+
+            return collection.updateOne(options.query, { $set: data })
+
+        }).catch(error => {
+
+            console.log(error)
+
+        })
+
+    }
+
+
     // · 
     aggregate(database, pipeline) {
+
+        database.schema = [this.namespace, database.schema].join('-')
+
         return this.connection.then(e => {
-            let schema = this.client.db(database.schema);
-            let collection = schema.collection(database.collection);
-            return collection.aggregate(pipeline).toArray();
+
+            let schema = this.client.db(database.schema)
+            let collection = schema.collection(database.collection)
+            return collection.aggregate(pipeline).toArray()
+
         }).catch(error => {
-            console.log(error);
-        });
+
+            console.log(error)
+
+        })
+
     }
+
+
     // · 
     aggregationPipelineSelection(query) {
+
         var pipeline = [{
-                // Processes multiple aggregation pipelines within a single stage on the same set of input documents.
-                "$facet": {
-                    // rows range found
-                    "rows": [
-                        { "$match": {} },
-                        { "$sort": { "datetime": -1 } },
-                        { "$skip": query.skip ? parseInt(query.skip) : 0 },
-                        { "$limit": query.limit ? parseInt(query.limit) : 100 },
-                    ],
-                    // total rows found
-                    "count": [{ "$count": "count" }]
-                }
-            }];
-        // Return records from specific topic
-        if (query.topic && query.topic != "") {
-            pipeline[0]["$facet"].rows[0]["$match"]["topic"] = query.topic;
+
+            // Processes multiple aggregation pipelines 
+            // within a single stage on the same set of input documents.
+            "$facet": {
+
+                // rows range found
+                "rows": [
+                    { "$match": {} },
+                    { "$sort": { "datetime": -1 } },
+                    { "$skip": query.skip ? parseInt(query.skip) : 0 },
+                    { "$limit": query.limit ? parseInt(query.limit) : 100 },
+                ],
+
+                // total rows found
+                "count": [{ "$count": "count" }]
+
+            }
+
+        }]
+
+
+        // Filter by specific field
+        if (query.field && query.field != "") {
+            pipeline[0]["$facet"].rows[0]["$match"][field] = query.text
         }
+
+
         // Return records from specific topic
         if ((query.start && query.start != "") || (query.end && query.end != "")) {
             pipeline[0]["$facet"].rows[0]["$match"]["datetime"] = {
                 "$gte": query.start ? parseInt(query.start) : 0,
                 "$lte": query.end ? parseInt(query.end) : Date.now(),
-            };
+            }
         }
+
         // Get N last records
         if (query.last && query.last != "") {
-            pipeline[0]["$facet"].rows[1]["$sort"].datetime = -1;
-            pipeline[0]["$facet"].rows[3]["$limit"] = parseInt(query.last);
+            pipeline[0]["$facet"].rows[1]["$sort"].datetime = -1
+            pipeline[0]["$facet"].rows[3]["$limit"] = parseInt(query.last)
         }
-        return pipeline;
+
+        return pipeline
+
     }
-    // · Find documents
-    findDocument(db, options = {}) {
-        db.schema = this.namespace + '-' + db.schema;
-        return this.connection.then(e => {
-            let schema = this.client.db(db.schema);
-            let collection = schema.collection(db.collection);
-            return collection
-                .find(options.query)
-                .project(options.fields)
-                .sort(options.sort)
-                .toArray();
-        }).catch(error => {
-            console.log(error);
-        });
-    }
-    updateDocument(db, options = {}, data = {}) {
-        db.schema = this.database + '-' + db.schema;
-        // use schema in config file if default collection is sent
-        if (db.schema == "default") {
-            db.schema = this.config.schema;
-        }
-        // Advanced search options
-        if (!options.query) {
-            options.query = {};
-        }
-        // Find by object id
-        if (options.query.objectid) {
-            options.query = {
-                '_id': ObjectId(options.query.objectid)
-            };
-        }
-        return this.connection.then(e => {
-            let schema = this.client.db(db.schema);
-            let collection = schema.collection(db.collection);
-            return collection.update(options.query, data);
-        }).catch(error => {
-            console.log(error);
-        });
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // · 
     insertDocument(db, data) {
 
-        db.schema = this.database + '-' + db.schema;
+        db.schema = this.database + '-' + db.schema
         if (!this.config.enabled) {
             return new Promise((resolve, reject) => {
-                return reject({ message: 'Database is disable' });
-            });
+                return reject({ message: 'Database is disable' })
+            })
         }
         return this.connection.then(e => {
-            let schema = this.client.db(db.schema);
-            let collection = schema.collection(db.collection);
-            data.datetime = new Date();
-            return collection.insertOne(data);
+            let schema = this.client.db(db.schema)
+            let collection = schema.collection(db.collection)
+            data.datetime = new Date()
+            return collection.insertOne(data)
         }).catch(error => {
-            console.log(error);
-        });
+            console.log(error)
+        })
     }
     // · 
     getSchemaStats(db) {
-        db.schema = this.database + '-' + db.schema;
+        db.schema = this.database + '-' + db.schema
         return (() => __awaiter(this, void 0, void 0, function* () {
-            let client = yield MongoClient.connect("mongodb://localhost:" + this.config.port, { useNewUrlParser: true });
-            let schema = client.db(db.schema);
-            let result = yield schema.stats();
+            let client = yield MongoClient.connect("mongodb://localhost:" + this.config.port, { useNewUrlParser: true })
+            let schema = client.db(db.schema)
+            let result = yield schema.stats()
             if (client) {
-                client.close();
+                client.close()
             }
             return new Promise((resolve, reject) => {
-                return resolve(result);
-            });
-        }))();
+                return resolve(result)
+            })
+        }))()
     }
     // · 
     getSchemaCollections(db) {
-        db.schema = this.database + '-' + db.schema;
+        db.schema = this.database + '-' + db.schema
         return this.connection.then(e => {
-            let schema = this.client.db(db.schema);
-            let collection = schema.listCollections();
+            let schema = this.client.db(db.schema)
+            let collection = schema.listCollections()
             return new Promise((resolve, reject) => {
-                return resolve(collection.toArray());
-            });
+                return resolve(collection.toArray())
+            })
         }).catch(error => {
-            console.log(error);
-        });
+            console.log(error)
+        })
     }
     // · 
     getSchemaCollectionStats(db) {
-        db.schema = this.database + '-' + db.schema;
+        db.schema = this.database + '-' + db.schema
         return (() => __awaiter(this, void 0, void 0, function* () {
-            let client = yield MongoClient.connect("mongodb://localhost:" + this.config.port, { useNewUrlParser: true });
-            let schema = client.db(db.schema);
-            let collection = schema.collection(db.collection);
-            let result = yield collection.stats();
+            let client = yield MongoClient.connect("mongodb://localhost:" + this.config.port, { useNewUrlParser: true })
+            let schema = client.db(db.schema)
+            let collection = schema.collection(db.collection)
+            let result = yield collection.stats()
             if (client) {
-                client.close();
+                client.close()
             }
             return new Promise((resolve, reject) => {
-                return resolve(result);
-            });
-        }))();
+                return resolve(result)
+            })
+        }))()
     }
     // · 
     deleteSchemaCollection(db) {
-        db.schema = this.database + '-' + db.schema;
+        db.schema = this.database + '-' + db.schema
         return (() => __awaiter(this, void 0, void 0, function* () {
-            let client = yield MongoClient.connect("mongodb://localhost:" + this.config.port, { useNewUrlParser: true });
-            let schema = client.db(db.schema);
-            let collection = schema.collection(db.collection);
-            let result = yield collection.drop();
+            let client = yield MongoClient.connect("mongodb://localhost:" + this.config.port, { useNewUrlParser: true })
+            let schema = client.db(db.schema)
+            let collection = schema.collection(db.collection)
+            let result = yield collection.drop()
             if (client) {
-                client.close();
+                client.close()
             }
             return new Promise((resolve, reject) => {
-                return resolve(result);
-            });
-        }))();
+                return resolve(result)
+            })
+        }))()
     }
     // · 
     aggregationPipelineCounting(query) {
-        let fields = ['value'];
+        let fields = ['value']
         if (query.fields) {
-            fields = query.fields;
+            fields = query.fields
         }
         var pipeline = [{
                 "$facet": {
@@ -307,27 +405,27 @@ class databaseService {
                             }
                         }]
                 }
-            }];
+            }]
         fields.forEach(field => {
-            pipeline[0]['$facet'].year[0]['$group'][field] = { $sum: `$data.${field}` };
-            pipeline[0]['$facet'].month[0]['$group'][field] = { $sum: `$data.${field}` };
-            pipeline[0]['$facet'].day[0]['$group'][field] = { $sum: `$data.${field}` };
-        });
-        return pipeline;
+            pipeline[0]['$facet'].year[0]['$group'][field] = { $sum: `$data.${field}` }
+            pipeline[0]['$facet'].month[0]['$group'][field] = { $sum: `$data.${field}` }
+            pipeline[0]['$facet'].day[0]['$group'][field] = { $sum: `$data.${field}` }
+        })
+        return pipeline
     }
     // · 
     runCommand(command) {
         return (() => __awaiter(this, void 0, void 0, function* () {
-            let client = yield MongoClient.connect("mongodb://localhost:" + this.config.port);
-            let schema = client.db('localschema');
-            let result = yield schema.command("show collections");
+            let client = yield MongoClient.connect("mongodb://localhost:" + this.config.port)
+            let schema = client.db('localschema')
+            let result = yield schema.command("show collections")
             if (client) {
-                client.close();
+                client.close()
             }
             return new Promise((resolve, reject) => {
-                return resolve(result);
-            });
-        }))();
+                return resolve(result)
+            })
+        }))()
     }
 }
 
