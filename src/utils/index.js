42065289a4a118a8b71ee2827d7ea8be4b4e6424
Luis Donis <ldonis.emc2@gmail.com>
2021-06-08 13:08:41 -0600

    feat: add unit conversion tools

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,45 +1,6 @@
 
+const { bytes_to_human } = require("./values_converter")
+const aggregation_pipeline_query = require("./aggregation_pipeline_query")
 
-exports.converter = {
-    bytes_to_human: function (size, unit = "bytes") {
-
-        if (!size || size < 0) {
-            size = 0
-        }
-
-        var decimals = 2
-        var units = {
-            bytes: "bytes",
-            kilobytes: "kilobytes",
-            megabytes: "megabytes",
-            gigabytes: "gigabytes"
-        }
-
-        // converts bytes to kilobytes
-        if (unit == units.bytes && size > 1024) {
-            return this.bytes_to_human(size / 1024, units.kilobytes)
-        }
-
-        // converts kilobytes to megabytes
-        if (unit == units.kilobytes && size > 1024) {
-            
-            return this.bytes_to_human(size / 1024, units.megabytes)
-        }
-
-        // converts megabytes to gigabyte
-        if (unit == units.megabytes && size > 1024) {
-            
-            return this.bytes_to_human(size / 1024, units.gigabytes)
-        }
-
-        // return size with no decimals
-        if (size.toFixed(decimals) % 1 === 0) {
-            return [size, unit].join(" ")
-        }
-
-        // return size with decimals
-        return [size.toFixed(decimals), unit].join(" ")
-
-    }
-
-}
+exports.bytes_to_human = bytes_to_human
+exports.aggregation_pipeline_query = aggregation_pipeline_query
---
+++ src/utils/values_converter.js
@@ -0,0 +1,41 @@
+
+exports.bytes_to_human = function (size, unit = "bytes") {
+
+    if (!size || size < 0) {
+        size = 0
+    }
+
+    var decimals = 2
+    var units = {
+        bytes: "bytes",
+        kilobytes: "kilobytes",
+        megabytes: "megabytes",
+        gigabytes: "gigabytes"
+    }
+
+    // converts bytes to kilobytes
+    if (unit == units.bytes && size > 1024) {
+        return this.bytes_to_human(size / 1024, units.kilobytes)
+    }
+
+    // converts kilobytes to megabytes
+    if (unit == units.kilobytes && size > 1024) {
+        
+        return this.bytes_to_human(size / 1024, units.megabytes)
+    }
+
+    // converts megabytes to gigabyte
+    if (unit == units.megabytes && size > 1024) {
+        
+        return this.bytes_to_human(size / 1024, units.gigabytes)
+    }
+
+    // return size with no decimals
+    if (size.toFixed(decimals) % 1 === 0) {
+        return [size, unit].join(" ")
+    }
+
+    // return size with decimals
+    return [size.toFixed(decimals), unit].join(" ")
+
+}
